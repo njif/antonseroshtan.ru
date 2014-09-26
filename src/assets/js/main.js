8da49a49d64b82b5f945c4d697f0d840b651a3b5
Anton Seroshtan <anton.seroshtan@gmail.com>
2014-09-26 10:01:07 +0400

    Small javascript refactoring

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,19 +1,21 @@
 $(document).ready(function() {
 
+	/* --- Variables --- */
+
 	var screen = 0,
 		container = $('.maincontent'),
 		pages = $('.page-section'),
 		inscroll = false,
-		touchStartY = 0;
+		touchStartY = 0,
+		timer = null;
 
-	$('body').on('touchstart', function(event) {
-		var touch = event.originalEvent.touches[0] || event.originalEvent.changedTouches[0];
-		touchStartY = touch.pageY;
-	});
+	/* --- Helper functions --- */
 
 	var isSwipeDown = function (event) {
+		if (!event)
+			return false;
 		if (event.deltaY != null)
-			return event.deltaY > 0;
+			return event.deltaY < 0;
 		if (event.originalEvent)
 		{
 			var touch = event.originalEvent.touches[0] || event.originalEvent.changedTouches[0];
@@ -24,37 +26,51 @@ $(document).ready(function() {
 		return false;
 	};
 
-	$('.page-section:first-child').addClass('active');
+	var checkInScroll = function() {
+		if (inscroll)
+			return true;
 
-	$('body').on('mousewheel touchmove', function(event) {
-
-		var activePage = pages.filter('.active');
-
-		if (!inscroll) {
-			inscroll = true;
-
-			if (isSwipeDown(event) > 0) {
-
-				if (activePage.prev().length) {
-					screen--;
-				}
-
-			} else {
-
-				if (activePage.next().length) {
-					screen++;
-				}
-			}
-		}
-
-		var position = (-screen * 100) + '%';
-
-		pages.eq(screen).addClass('active').siblings().removeClass('active');
-		container.css('top', position);
-
+		inscroll = true;
 		setTimeout(function(){
 			inscroll = false;
 		}, 1300);
+	};
+
+	var setCurrentScreen = function(event) {
+
+		var activePage = pages.filter('.active');
+		if (isSwipeDown(event)) {
+
+			if (activePage.next().length) {
+				screen++;
+			}
+
+		} else {
+
+			if (activePage.prev().length) {
+				screen--;
+			}
+		}
+
+		pages.eq(screen).addClass('active').siblings().removeClass('active');
+	};
+
+	/* --- Setup --- */
+
+	$('.page-section:first-child').addClass('active');
+
+	/* --- Attach events --- */
+
+	$('body').on('touchstart', function(event) {
+		var touch = event.originalEvent.touches[0] || event.originalEvent.changedTouches[0];
+		touchStartY = touch.pageY;
+	}).on('mousewheel touchmove', function(event) {
+		if (checkInScroll())
+			return;
+		setCurrentScreen(event);
+
+		var position = (-screen * 100) + '%';
+		container.css('top', position);
 	});
 
 });
